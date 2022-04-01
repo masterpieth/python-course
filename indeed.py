68f142e7f9545154da67a40653c93df77541a4f3
NA YEONG KIM <kny0339@gmail.com>
2022-04-01 12:41:16 +0900

    indeed scraper
    kr.indeed.com job scraper

---
+++ indeed.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+SEARCH_CONDITION = "/jobs?q=python&limit="
+INDEED_URL = "https://kr.indeed.com"
+
+def extract_indeed_pages():
+  result = requests.get(INDEED_URL + SEARCH_CONDITION + str(LIMIT))
+
+  soup = BeautifulSoup(result.text, "html.parser")
+  
+  pagination = soup.find("div",{"class":"pagination"})
+  
+  links = pagination.find_all("a")
+  pages = []
+
+  for link in links[:-1]:
+   pages.append(int(link.string))
+
+  max_page = pages[-1]
+
+  return max_page
+
+def extract_job(html):
+  
+  title = html.find("h2",{"class":"jobTitle"}).find_all("span")[-1].string
+  company_name = html.find("span",{"class":"companyName"}).string
+  company_location = html.find("div",{"class":"companyLocation"}).string
+  direct_url = html['href']
+  
+  return {'title' : title, 'company_name' : company_name, 'company_location' : company_location, 'direct_url' : INDEED_URL + direct_url}
+  
+
+def extract_indeed_jobs(last_page):
+
+  jobs = []
+  
+  for page in range(last_page):
+    result = requests.get(f"{INDEED_URL}{SEARCH_CONDITION}&start={page*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    htmls = soup.find("div", {"id":"mosaic-provider-jobcards"}).find_all("a")
+    
+    for html in htmls:
+      try:
+        jobs.append(extract_job(html))
+      except:
+        continue
+  print(jobs)
+  return jobs
